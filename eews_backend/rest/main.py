279b22ec0c67a9d013223db53f9efafaaf2bbb17
Zsaschz <fathanmuthahhari@gmail.com>
2023-07-27 09:19:20 +0700

    attempt to fix missing timestamp from influx

--- eews_backend/rest/main.py
+++ eews_backend/rest/main.py
@@ -1,9 +1,19 @@
 from datetime import timedelta
 from math import ceil
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Response, WebSocket, WebSocketDisconnect,  status, UploadFile, BackgroundTasks
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Response,
+    WebSocket,
+    WebSocketDisconnect,
+    status,
+    UploadFile,
+    BackgroundTasks,
+)
 from fastapi.params import Body
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.staticfiles import StaticFiles
 from typing import Dict, List
 from influxdb_client import Point, WritePrecision
@@ -12,9 +22,7 @@ from logging.config import dictConfig
 
 from database.mongodb import *
 from database.influxdb import *
-from stream_processing.schema import *
-from stream_processing.kafka import KafkaProducer, BOOTSTRAP_SERVER
-from stream_processing.topics import PREPROCESSED_TOPIC
+from stream import KafkaProducer, PREPROCESSED_TOPIC
 from utils import *
 from .model import *
 from .websocket import ConnectionManager
@@ -75,10 +83,13 @@ HTML = """
     </body>
 </html>
 """
+
+
 @app.get("/test")
 async def test():
     query_api = client.query_api()
-    now = datetime(2015,8,20,15,12,1)
+    now = datetime(2015, 8, 20, 15, 12, 1)
+    first_starttime = now
     query = f"""
     from(bucket: "eews") 
         |> range(start: {(now - timedelta(seconds=1)).isoformat()}Z, stop: {now.isoformat()}Z) 
@@ -89,12 +100,13 @@ async def test():
     # log.debug(data.columns)
     # log.debug(data.head(50))
     data = data.fillna(0)
-    return data.to_dict() 
+    return data.to_dict()
 
 
 @app.get("/")
 async def get():
     return HTMLResponse(HTML)
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -102,7 +114,7 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         query_api = client.query_api()
         now = datetime.now()
-        now = datetime(2015,8,20,15,12,1)
+        # now = datetime(2015, 8, 20, 15, 12, 1)
         while True:
             await asyncio.sleep(1)
             query = f"""
@@ -111,6 +123,18 @@ async def websocket_endpoint(websocket: WebSocket):
                 |> filter(fn: (r) => r["_measurement"] == "seismograf") 
                 |> pivot(rowKey: ["_time"], columnKey: ["channel", "station"], valueColumn: "_value")"""
             data: pd.DataFrame = query_api.query_data_frame(query=query)
+            time_list = [
+                _time.isoformat(sep=" ", timespec="microseconds")[:-6]
+                for _time in data["_time"].to_list()
+            ]
+            for i in range(25):
+                timestamp = (
+                    now + timedelta(seconds=(i * 0.04)) - timedelta(seconds=1)
+                ).isoformat(sep=" ", timespec="microseconds")
+                if timestamp not in time_list:
+                    print(timestamp, time_list)
+                    # data.add(pd.DataFrame())
+
             log.debug(data)
             json_data = data.to_json()
             now += timedelta(seconds=1)
@@ -118,23 +142,30 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
+
 @app.get("/station", response_model=List[StationModel])
 async def list_seismometer():
     list_data = await db["station"].find().to_list(1000000000)
     log.info(list_data)
     return list_data
 
+
 @app.get("/station/{name}", response_model=StationModel)
 async def get_seismometer(name: str):
     data = await db["station"].find_one({"name": name})
     if data is not None:
         return data
-    raise HTTPException(status_code=404, detail=f"Seismometer with name {name} not found")
+    raise HTTPException(
+        status_code=404, detail=f"Seismometer with name {name} not found"
+    )
+
 
 @app.put("/station/{name}", response_model=StationModel)
-async def update_seismometer(name: str, background_task: BackgroundTasks, data: UpdateStationModel = Body(...)):
+async def update_seismometer(
+    name: str, background_task: BackgroundTasks, data: UpdateStationModel = Body(...)
+):
     data = data.model_dump()
-    
+
     if len(data) >= 1:
         update_result = await db["station"].update_one({"name": name}, {"$set": data})
 
@@ -149,23 +180,36 @@ async def update_seismometer(name: str, background_task: BackgroundTasks, data: 
         await adjust_closest_stations()
         return existing_data
 
-    raise HTTPException(status_code=404, detail=f"Seismometer with name {name} not found")
+    raise HTTPException(
+        status_code=404, detail=f"Seismometer with name {name} not found"
+    )
+
 
 @app.post("/station", response_model=StationModel, status_code=status.HTTP_201_CREATED)
-async def create_seismometer(background_task: BackgroundTasks, data: UpdateStationModel = Body(...)):
+async def create_seismometer(
+    background_task: BackgroundTasks, data: UpdateStationModel = Body(...)
+):
     data = data.model_dump()
-    if (existing_data := await db["station"].find_one({"name": data["name"]})) is not None:
-        raise HTTPException(status_code=400, detail=f"Seismometer with name {data['name']} already exists")
-    
+    if (
+        existing_data := await db["station"].find_one({"name": data["name"]})
+    ) is not None:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Seismometer with name {data['name']} already exists",
+        )
+
     all_stations = await db["station"].find().to_list(1000000000)
     calculated = dict()
-    
+
     data["closest_stations"] = calculate_closest_station(data, all_stations, calculated)
-    
+
     new_data = await db["station"].insert_one(data)
-    if (existing_data := await db["station"].find_one({"_id": new_data.inserted_id})) is not None:
+    if (
+        existing_data := await db["station"].find_one({"_id": new_data.inserted_id})
+    ) is not None:
         await adjust_closest_stations()
         return existing_data
+
 
 @app.delete("/station/{name}")
 async def delete_seismometer(name: str, background_task: BackgroundTasks):
@@ -175,7 +219,10 @@ async def delete_seismometer(name: str, background_task: BackgroundTasks):
         await adjust_closest_stations()
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-    raise HTTPException(status_code=404, detail=f"Seismometer with name {name} not found")
+    raise HTTPException(
+        status_code=404, detail=f"Seismometer with name {name} not found"
+    )
+
 
 @app.post("/mseed", status_code=status.HTTP_201_CREATED)
 async def upload_mseed(file: UploadFile, background_tasks: BackgroundTasks):
@@ -184,34 +231,44 @@ async def upload_mseed(file: UploadFile, background_tasks: BackgroundTasks):
     background_tasks.add_task(save_mseed, contents, filename)
     return {"file_size": file.size, "filename": filename}
 
+
 @measure_execution_time
-async def adjust_closest_stations(all_stations = None):
+async def adjust_closest_stations(all_stations=None):
     log.info("Adjusting closest stations")
     if not all_stations:
         all_stations = await db["station"].find().to_list(1000000000)
 
     calculated = dict()
-    
+
     for station in all_stations:
-        station["closest_stations"] = calculate_closest_station(station, all_stations, calculated)
+        station["closest_stations"] = calculate_closest_station(
+            station, all_stations, calculated
+        )
         await db["station"].update_one({"name": station["name"]}, {"$set": station})
-        
-def calculate_closest_station(curr_station: List[Dict], all_stations: List[Dict], calculated: Dict = None):
+
+
+def calculate_closest_station(
+    curr_station: List[Dict], all_stations: List[Dict], calculated: Dict = None
+):
     distances = []
-    
+
     for other_station in all_stations:
-        if other_station['name'] == curr_station["name"]:
+        if other_station["name"] == curr_station["name"]:
             continue
         distance = float("inf")
         if f"{other_station['name']}-{curr_station['name']}" in calculated:
             distance = calculated[f"{other_station['name']}-{curr_station['name']}"]
         else:
-            distance = hs.haversine((curr_station['x'], curr_station['y']), (other_station['x'], other_station['y']))
+            distance = hs.haversine(
+                (curr_station["x"], curr_station["y"]),
+                (other_station["x"], other_station["y"]),
+            )
             calculated[f"{curr_station['name']}-{other_station['name']}"] = distance
-        distances.append((other_station['name'], distance))
-    
+        distances.append((other_station["name"], distance))
+
     distances.sort(key=lambda x: x[1])
     return [i[0] for i in distances[:3]]
+
 
 @measure_execution_time
 def save_mseed(contents: bytes, filename: str):
@@ -219,43 +276,56 @@ def save_mseed(contents: bytes, filename: str):
     filepath = f"{MODULE_DIR}{STATIC_DIR}{filename}"
     with open(filepath, "wb") as f:
         f.write(contents)
-        
+
     records = []
     events = []
     traces = process_data(filepath)
     start = time.monotonic_ns()
     for mseed_data in traces:
-        starttime: datetime = UTCDateTime(mseed_data['starttime']).datetime
-        endtime = UTCDateTime(mseed_data['endtime']).datetime
-        delta = 1/int(mseed_data['sampling_rate'])
+        starttime: datetime = UTCDateTime(mseed_data["starttime"]).datetime
+        endtime = UTCDateTime(mseed_data["endtime"]).datetime
+        delta = 1 / int(mseed_data["sampling_rate"])
         channel = mseed_data["channel"]
         station = mseed_data["station"]
         first_starttime = nearest_datetime_rounded(starttime, delta * 10**6)
-        
-        log.debug(f"Processing {station}_{channel} from {filename} with len {len(mseed_data['data_interpolated'])}")
-        
-        for data_point in mseed_data['data_interpolated']: 
-            point = Point("seismograf").time(first_starttime, write_precision=WritePrecision.MS).tag("channel", channel).tag("station", station).field("data", data_point)
-            records.append(point)      
+
+        log.debug(
+            f"Processing {station}_{channel} from {filename} with len {len(mseed_data['data_interpolated'])}"
+        )
+
+        for data_point in mseed_data["data_interpolated"]:
+            point = (
+                Point("seismograf")
+                .time(first_starttime, write_precision=WritePrecision.MS)
+                .tag("channel", channel)
+                .tag("station", station)
+                .field("data", data_point)
+            )
+            records.append(point)
             event = {
                 "station": station,
                 "channel": channel,
                 "time": str(starttime),
-                "data": data_point
+                "data": data_point,
             }
             events.append(event)
             first_starttime += timedelta(seconds=delta)
-    
-    with InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG) as client:
+
+    with InfluxDBClient(
+        url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG
+    ) as client:
         with client.write_api() as writer:
             log.debug("Start batch save to InfluxDB")
             writer.write(bucket="eews", record=records)
-    
+
     log.debug("Start producing events")
-    for i in range(len(events)):    
+    for i in range(len(events)):
         producer.produce_message(events[i])
-    
-    log.debug(f"Finished process mseed with {len(records)} data for {(time.monotonic_ns() - start) / 10**9}s with rate of {len(records)/((time.monotonic_ns() - start) / 10**9)}")
+
+    log.debug(
+        f"Finished process mseed with {len(records)} data for {(time.monotonic_ns() - start) / 10**9}s with rate of {len(records)/((time.monotonic_ns() - start) / 10**9)}"
+    )
+
 
 @measure_execution_time
 def process_data(mseed_filename: str):
@@ -267,22 +337,23 @@ def process_data(mseed_filename: str):
         lowcut = 1.0
         highcut = 5.0
         order = 5
-        preprocessed['network'] = detail.stats.network
-        preprocessed['station'] = detail.stats.station
-        preprocessed['channel'] = detail.stats.channel
-        preprocessed['location'] = detail.stats.location
-        preprocessed['starttime'] = str(detail.stats.starttime)
-        preprocessed['endtime'] = str(detail.stats.endtime)
-        preprocessed['delta'] = detail.stats.delta
-        preprocessed['npts'] = detail.stats.npts
-        preprocessed['calib'] = detail.stats.calib
-        preprocessed['data'] = detail.data
+        preprocessed["network"] = detail.stats.network
+        preprocessed["station"] = detail.stats.station
+        preprocessed["channel"] = detail.stats.channel
+        preprocessed["location"] = detail.stats.location
+        preprocessed["starttime"] = str(detail.stats.starttime)
+        preprocessed["endtime"] = str(detail.stats.endtime)
+        preprocessed["delta"] = detail.stats.delta
+        preprocessed["npts"] = detail.stats.npts
+        preprocessed["calib"] = detail.stats.calib
+        preprocessed["data"] = detail.data
         data_before = detail.data
         data_processed = butter_bandpass_filter(data_before, lowcut, highcut, fs, order)
         data_to = list(data_processed)
-        data_to = letInterpolate(data_to, int(ceil(len(data_to)*25/detail.stats.sampling_rate)))
-        preprocessed['sampling_rate'] = 25.0
-        preprocessed['data_interpolated'] = data_to
+        data_to = letInterpolate(
+            data_to, int(ceil(len(data_to) * 25 / detail.stats.sampling_rate))
+        )
+        preprocessed["sampling_rate"] = 25.0
+        preprocessed["data_interpolated"] = data_to
         traces.append(preprocessed)
     return traces
- 
